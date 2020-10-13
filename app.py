fcb79e6885718d1b67c49b96a3e7fc60d0b91416
msaad1311 <msaad1311@users.noreply.github.com>
2020-10-12 23:47:17 -0400

    made changes to the html file to allow background and added key pair for AWS
    In the html code there were issues in terms of having the background for the application, so that was being solved and then a key pair is being added for AWS deployment

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import pandas as pd
 app = Flask(__name__,template_folder='templates')
 data = pd.read_excel('Preprocessed File.xls')
 data=data.loc[:, ~data.columns.str.contains('^Unnamed')]
-# print(data['original_title']=='Avatar')
 
 @app.route("/",methods=['GET','POST'])
 @cross_origin()
@@ -16,13 +15,11 @@ def main():
     if request.method=='POST':
         movie_name = request.form['movie_name']
         movie_name=movie_name.title()
-        print(movie_name)
         if movie_name not in data['original_title'].unique():
             return render_template('negative.html')
         else:
             res=Recommendation.results(movie_name)
-            return render_template('positive.html',movie_names=res['Title'].tolist(),
-            director=res['Director'].tolist(),search_name=movie_name)
+            return render_template('positive.html',movie_names=res['Title'].tolist(),director=res['Director'].tolist(),search_name=movie_name)
 
 if __name__ == "__main__":
     app.run(debug=True)

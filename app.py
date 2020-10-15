051a906b756059ba924eba751abe2ba56cf7568e
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2020-10-15 11:38:58 -0400

    made some adjustments in app.py

--- app.py
+++ app.py
@@ -7,17 +7,37 @@ app = Flask(__name__, template_folder='templates')
 @app.route("/", methods=['GET', 'POST'])
 @cross_origin()
 def main():
+    data = reader()
+    sim_model = transformer(data,False,True)
     if request.method == 'GET':
         return render_template('index.html')
+
     if request.method == 'POST':
         movie_name = request.form['movie_name']
         movie_name = movie_name.title()
-        res = results(movie_name)
-        if not isinstance(res, pd.DataFrame):
+        
+        if movie_name not in data['original_title'].unique():
             return render_template('negative.html')
         else:
+            res=recommendations(movie_name,data,sim_model)
             return render_template('positive.html', movie_names=res['Title'].tolist(), 
             director=res['Director'].tolist(), search_name=movie_name)
+
+
+# @app.route("/", methods=['GET', 'POST'])
+# @cross_origin()
+# def main():
+#     if request.method == 'GET':
+#         return render_template('index.html')
+#     if request.method == 'POST':
+#         movie_name = request.form['movie_name']
+#         movie_name = movie_name.title()
+#         res = results(movie_name)
+#         if not isinstance(res, pd.DataFrame):
+#             return render_template('negative.html')
+#         else:
+#             return render_template('positive.html', movie_names=res['Title'].tolist(), 
+#             director=res['Director'].tolist(), search_name=movie_name)
 
 
 if __name__ == "__main__":
